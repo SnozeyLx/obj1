696e750c6b4803dc28f95815b0ef49d4cc610fbf
SnozeyLx <richard_rd20@hotmail.com>
2021-10-03 15:16:32 -0300

    conflito "04"

--- grilo.java
+++ grilo.java
@@ -1,3 +1,10 @@
 public class grilo {
     int pulo;
+    String nome;
+
+    public String saltar(String s) {
+        this.nome = s;
+
+        return "foi";
+    }
 }
--- main.java
+++ main.java
@@ -2,4 +2,9 @@ public class main {
     int x = 10;
 
     grilo g = new grilo();
+
+    public void run() {
+        g.saltar("saltou");
+        System.out.println(g.saltar("salou"));
+    }
 }
